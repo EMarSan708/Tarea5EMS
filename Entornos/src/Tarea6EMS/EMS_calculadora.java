fd71df45285cec86626453ee6d6bf34666c40bec
diurno <diurno@pc144-13>
2022-01-28 13:35:45 +0100

    Prueba
    Change-Id: Ie09ab5204c2d16015149e24abae4e305a4fe0af0

---
+++ Entornos/src/Tarea6EMS/EMS_calculadora.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package Tarea6EMS;
+
+/**
+ * @author Juan Simón
+ *
+ */
+public class EMS_calculadora {
+	
+	public EMS_calculadora(){
+        
+    }
+    
+    public int suma(int num1,int num2){
+        return num1+num2;
+    }
+    
+    public int resta(int num1,int num2){
+        return num1-num2;
+    }
+    
+    public int multiplica(int num1,int num2){
+        return num1*num2;
+    }
+    
+    public int divide(int num1,int num2){
+        if (num2==0) throw new ArithmeticException("NO PUEDES DIVIDIR POR CERO, MELON");
+        return num1/num2;
+    }
+
+	private int valor1;
+	private int valor2;
+}
